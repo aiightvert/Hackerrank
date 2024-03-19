7eca7c1a9151f82c8aea21b2b096fda14a1ff06e
Dennis Imanuel <127971950+aiightvert@users.noreply.github.com>
2024-03-20 03:02:56 +0700

    Add files via upload

---
+++ Basic Python/Basic List/Basic Lists.py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+    N = int(input())
+    arr = []
+    for i in range(N):
+        command = input().split() # Split the command input (list)
+        
+        if command[0] == "insert":
+            arr.insert(int(command[1]),int(command[2]))
+            
+        elif command[0] == "print":
+            print(arr)
+        
+        elif command[0] == "remove":
+            arr.remove(int(command[1]))
+        
+        elif command[0] == "append":
+            arr.append(int(command[1]))
+        
+        elif command[0] == "sort":
+            arr.sort()
+        
+        elif command[0] == "pop":
+            arr.pop()
+        
+        elif command[0] == "reverse":
+            arr.reverse()
